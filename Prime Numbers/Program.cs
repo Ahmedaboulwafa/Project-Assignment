4d547cadf3588fec0b354f3146a7791ef27b1260
Ahmedaboulwafa <120792414+Ahmedaboulwafa@users.noreply.github.com>
2022-12-17 17:40:49 +0200

    Add files via upload

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Numbers From");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("To");
+            int y = int.Parse(Console.ReadLine());
+            if (x==0 || x==1)
+            {
+                x = 2;
+            }
+            Console.WriteLine("Prime Numbers Between This Numbers Is:");
+            for (; x <= y; x++)
+            {
+                int z = 0;
+                for (int i = 2; i < x; i++)
+                {
+                    if (x % i ==0)
+                    {
+                        z = 1; 
+                       break;
+                    }
+                   
+                }
+                if (z!=1)
+                {
+                    Console.WriteLine(x);
+                }
+
+            }
+        }
+    }
+}
