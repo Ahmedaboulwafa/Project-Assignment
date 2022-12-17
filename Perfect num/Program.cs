d37be00a7dd67714332dfddd044b83df6ac5258c
Ahmedaboulwafa <120792414+Ahmedaboulwafa@users.noreply.github.com>
2022-12-17 17:41:53 +0200

    Add files via upload

---
+++ Perfect num/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Perfect_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Numbers From");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("To");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Perfect Numbers Between This Numbers Is:");
+            for (; x <= y; x++)
+            {
+                int sum = 0;
+                for (int i = 1; i < x; i++)
+                {
+                    if (x % i == 0 )
+                    {
+                        sum += i;
+
+                    }
+                }
+                if (x==sum)
+                {
+                    Console.WriteLine(x);
+                }
+               
+            }
+        }
+    }
+}
